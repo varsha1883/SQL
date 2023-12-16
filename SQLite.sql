0450848d0705dda9a39776d422621c377a94607b
Varsha Vishwakarma <49994816+varsha1883@users.noreply.github.com>
2023-12-16 16:43:21 -0500

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,14 @@
+SELECT 
+    DATE(transaction_time) AS date,
+    AVG(SUM(transaction_amount)) OVER (
+        ORDER BY DATE(transaction_time)
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS rolling_avg
+FROM 
+    transactions
+GROUP BY 
+    DATE(transaction_time)
+HAVING 
+    DATE(transaction_time) <= '2021-01-31'
+ORDER BY 
+    date;
